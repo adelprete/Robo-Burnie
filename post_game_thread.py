df8fed7f153045af3ed0eee22e4189a15564fe3d
Anthony Delprete <adelprete87@gmail.com>
2019-12-28 12:55:11 -0500

    Post Game Thread and Standings script

---
+++ post_game_thread.py
@@ -0,0 +1,104 @@
+import requests
+import praw
+import time
+from datetime import datetime, timedelta
+from settings import TEAM, TEAM_NAMES
+from private import BOT_PASSWORD, CLIENT_SECRET_KEY, CLIENT_ID
+
+def get_todays_game():
+    today = datetime.utcnow() - timedelta(hours=5)
+    games = requests.get(f'https://data.nba.net/prod/v2/{today.strftime("%Y")}{today.strftime("%m")}{today.strftime("%d")}/scoreboard.json').json()
+    
+    todays_game = {}
+    for game in games['games']:
+        if game['vTeam']['triCode'] == TEAM or game['hTeam']['triCode'] == TEAM:
+            todays_game = game
+    
+    return todays_game
+
+def get_boxscore(gameid):
+    today = datetime.utcnow() - timedelta(hours=5)
+    boxscore = requests.get(f'https://data.nba.net/data/10s/prod/v1/{today.strftime("%Y")}{today.strftime("%m")}{today.strftime("%d")}/{gameid}_boxscore.json').json()
+    return boxscore
+
+if __name__ == '__main__':
+    while True:
+        todays_game = get_todays_game()
+        
+        if todays_game == {} or todays_game.get('statusNum') == 3:
+            print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game ended waiting 15 hrs to check for next game')
+            time.sleep(54000)
+            continue 
+        print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Checked if game started')
+        while todays_game['statusNum'] in [2,3]:
+            todays_game = get_todays_game()
+            if todays_game['statusNum'] == 2 or (todays_game['period']['current'] >= 4 and todays_game['statusNum'] == 2 and todays_game['isEndOfPeriod'] and todays_game['vTeam']['score'] != todays_game['hTeam']['score']):
+                team_stats_key = 'hTeam'
+                opponent_stats_key = 'vTeam'
+                if todays_game['vTeam']['triCode'] == TEAM:
+                    team_stats_key = 'hTeam'
+                    opponent_stats_key = 'hTeam'
+                boxscore = get_boxscore(todays_game['gameId'])
+                reddit = praw.Reddit(client_id=CLIENT_ID,
+                                 client_secret=CLIENT_SECRET_KEY,
+                                 password=BOT_PASSWORD,
+                                 user_agent='Game Bot by BobbaGanush87',
+                                 username='bobbaganush87')
+                
+                # Grab general score information
+                team_score = todays_game[team_stats_key]['score']
+                opponents_score = todays_game[opponent_stats_key]['score']
+                opponents_name = todays_game[opponent_stats_key]['triCode']
+                
+                # Grab points leader information
+                points_statline = '\n*'
+                points_leader_value = boxscore['stats'][team_stats_key]['leaders']['points']['value']
+                for index, player in enumerate(boxscore['stats'][team_stats_key]['leaders']['points']['players']):
+                    if index > 0:
+                        points_statline += ' /'
+                    points_statline += f' **{player["firstName"]} {player["lastName"]}**'
+                points_statline += f': {points_leader_value} PTS'
+                
+                # Grab reboundss leader information
+                rebounds_statline = '\n*'
+                rebounds_leader_value = boxscore['stats'][team_stats_key]['leaders']['rebounds']['value']
+                for index, player in enumerate(boxscore['stats'][team_stats_key]['leaders']['rebounds']['players']):
+                    if index > 0:
+                        assists_statline += ' /'
+                    rebounds_statline += f' **{player["firstName"]} {player["lastName"]}**'
+                rebounds_statline += f': {rebounds_leader_value} REBS'
+                
+                # Grab assists leader information
+                assists_statline = '\n*'
+                assists_leader_value = boxscore['stats'][team_stats_key]['leaders']['assists']['value']
+                for index, player in enumerate(boxscore['stats'][team_stats_key]['leaders']['assists']['players']):
+                    if index > 0:
+                        assists_statline += ' /'
+                    assists_statline += f' **{player["firstName"]} {player["lastName"]}**'
+                assists_statline += f': {assists_leader_value} ASTS'
+                
+                if team_score < opponents_score:
+                    result = 'lose to'
+                else:
+                    result = 'defeat'
+                
+                title = f'[Post Game Thread] {TEAM_NAMES[TEAM]} {result} {TEAM_NAMES[opponents_name]} {team_score} - {opponents_score}'
+                selftext = f'* [Box Score](https://www.nba.com/games/{todays_game["gameUrlCode"]}#/boxscore)'
+                selftext += points_statline
+                selftext += rebounds_statline
+                selftext += assists_statline
+                result = reddit.subreddit('testingground4bots').submit(title, selftext=selftext)#flair_id='d79dc9aa-cf0d-11e2-9b1b-12313d163d8f')
+                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game ended thread posted')
+                break
+            
+            if todays_game['period']['current'] >= 4 and todays_game['statusNum'] == 2 and not todays_game['clock']:
+                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game might have ended')
+                time.sleep(3)
+            elif todays_game['period']['current'] >= 4 and todays_game['clock'] and int(todays_game['clock'].replace(':', '').split('.')[0]) < 30:
+                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game is almost over')
+                time.sleep(10)
+            else:
+                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game not over yet')
+                time.sleep(90)
+        time.sleep(600)
+    
---
+++ standings.py
@@ -0,0 +1,89 @@
+import requests
+import praw
+from settings import TEAM
+from private import BOT_PASSWORD, CLIENT_ID, CLIENT_SECRET_KEY
+
+def get_todays_standings():
+    standings = requests.get(f'http://data.nba.net/data/10s/prod/v2/current/standings_conference.json').json()
+    return standings['league']['standard']['conference']['east']
+
+# The css on the custom widget gets saved as a giant string
+css_str = """
+p {
+padding: 12px;
+color: white;
+font-size: 10px;
+letter-spacing: .5px;
+background: #f3a03d;
+border-radius: 5px 5px 0px 0px;
+margin-bottom: 12px;
+font-family: Sans-Serif;
+}
+
+td, th{
+ padding: 3px 15px;
+}
+
+table tr:nth-child(1)>th{
+background: #dcdcdc
+}
+
+table tr:nth-child(1)>td,table tr:nth-child(2), table tr:nth-child(3), table tr:nth-child(4), table tr:nth-child(5), table tr:nth-child(6), table tr:nth-child(7), table tr:nth-child(8) {
+ background: #ffffd7
+}
+
+table {
+  border-collapse: collapse;
+}
+table th, table td{
+  border: .5px solid black; 
+}
+table tr:first-child th {
+  border-top: 0;
+}
+table tr td:first-child, table tr th:first-child{
+  border-left: 0;
+}
+table tr:last-child td,table tr:last-child th {
+  border-bottom: 0;
+}
+table tr td:last-child, table tr th:last-child{
+  border-right: 0;
+}
+"""
+
+
+if __name__ == '__main__':
+    """
+    This script will update the standings on the Miami Heat 'new' reddit.
+    """
+    
+    # Connect to Reddit
+    reddit = praw.Reddit(client_id=CLIENT_ID,
+                     client_secret=CLIENT_SECRET_KEY,
+                     password=BOT_PASSWORD,
+                     user_agent='Game Bot by BobbaGanush87',
+                     username='bobbaganush87')
+                     
+    # Find standings widget
+    widgets = reddit.subreddit('heat').widgets
+    standings_widget = None
+    for widget in widgets.sidebar:
+        if widget.shortName.lower() == 'standings':
+            standings_widget = widget
+            break
+    
+    # Get latest standings from NBA api and build Markdown
+    if standings_widget:
+        nba_standings = get_todays_standings()
+        standings_markdown = 'STANDINGS\n\n| | Team | W | L | Pct |\n|--|--|--|--|--|'
+        
+        for position, standing in enumerate(nba_standings, start=1):
+            team_nickname = f'{standing["teamSitesOnly"]["teamNickname"]}'
+            if standing["teamSitesOnly"]["teamTricode"] == TEAM:
+                team_nickname = f'**{standing["teamSitesOnly"]["teamNickname"]}**'
+            standing_markdown = f'\n| {position} |  {team_nickname} | {standing["win"]} | {standing["loss"]} | {standing["winPct"]}'
+            standings_markdown += standing_markdown
+        
+        standings_widget.mod.update(text=standings_markdown, css=css_str)
+    
