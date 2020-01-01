06fc59052f08244b7b9b6b56a35ed34819949a60
Anthony Delprete <adelprete87@gmail.com>
2020-01-01 11:56:35 -0500

    fix run.sh script

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "using virtual environment: " "/venv"
-python3 -m venv "venv"
-source venv/bin/activate
-pip3 install -r requirements.txt
-nohup python3 -m new_migrators.${1}.scripts ${@:2} &
