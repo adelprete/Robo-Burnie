069bbd21d73c3cadbf7e50f455acf61cd3fa4476
Anthony Delprete <adelprete87@gmail.com>
2020-01-01 22:48:50 -0500

    post_game_thread is python3.4 compatible

--- post_game_thread.py
+++ post_game_thread.py
@@ -7,7 +7,7 @@ from private import BOT_PASSWORD, CLIENT_SECRET_KEY, CLIENT_ID
 
 def get_todays_game():
     today = datetime.utcnow() - timedelta(hours=5)
-    games = requests.get(f'https://data.nba.net/prod/v2/{today.strftime("%Y")}{today.strftime("%m")}{today.strftime("%d")}/scoreboard.json').json()
+    games = requests.get('https://data.nba.net/prod/v2/{}{}{}/scoreboard.json'.format(today.strftime("%Y"), today.strftime("%m"), today.strftime("%d"))).json()
     
     todays_game = {}
     for game in games['games']:
@@ -18,18 +18,25 @@ def get_todays_game():
 
 def get_boxscore(gameid):
     today = datetime.utcnow() - timedelta(hours=5)
-    boxscore = requests.get(f'https://data.nba.net/data/10s/prod/v1/{today.strftime("%Y")}{today.strftime("%m")}{today.strftime("%d")}/{gameid}_boxscore.json').json()
+    boxscore = requests.get('https://data.nba.net/data/10s/prod/v1/{}{}{}/{}_boxscore.json'.format(today.strftime("%Y"), today.strftime("%m"), today.strftime("%d"), gameid)).json()
     return boxscore
 
 if __name__ == '__main__':
-    while True:
+    thread_posted = False
+    while thread_posted == False:
         todays_game = get_todays_game()
         
-        if todays_game == {} or todays_game.get('statusNum') == 3:
-            print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game ended waiting 15 hrs to check for next game')
-            time.sleep(54000)
-            continue 
-        print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Checked if game started')
+        if todays_game == {}:
+            print('[{}]: No Game Today'.format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
+            break
+        elif todays_game['statusNum'] == 3:
+            print('[{}]: Post Game Thread already Posted'.format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
+            break
+        
+        if todays_game['statusNum'] == 1:
+            print("[{}]: Game hasn't started yet".format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
+            time.sleep(5400)
+        
         while todays_game['statusNum'] in [2,3]:
             todays_game = get_todays_game()
             if todays_game['statusNum'] == 3 or (todays_game['period']['current'] >= 4 and todays_game['statusNum'] == 2 and todays_game['period']['isEndOfPeriod'] and todays_game['vTeam']['score'] != todays_game['hTeam']['score']):
@@ -51,56 +58,56 @@ if __name__ == '__main__':
                 opponents_name = todays_game[opponent_stats_key]['triCode']
                 
                 # Grab points leader information
-                points_statline = '\n'
+                points_statline = '\n\n'
                 points_leader_value = boxscore['stats'][team_stats_key]['leaders']['points']['value']
                 for index, player in enumerate(boxscore['stats'][team_stats_key]['leaders']['points']['players']):
                     if index > 0:
                         points_statline += ' /'
-                    points_statline += f' **{player["firstName"]} {player["lastName"]}**'
-                points_statline += f': {points_leader_value} PTS'
+                    points_statline += ' **{} {}**'.format(player["firstName"], player["lastName"])
+                points_statline += ': {} PTS'.format(points_leader_value)
                 
                 # Grab reboundss leader information
-                rebounds_statline = '\n'
+                rebounds_statline = '\n\n'
                 rebounds_leader_value = boxscore['stats'][team_stats_key]['leaders']['rebounds']['value']
                 for index, player in enumerate(boxscore['stats'][team_stats_key]['leaders']['rebounds']['players']):
                     if index > 0:
                         rebounds_statline += ' /'
-                    rebounds_statline += f' **{player["firstName"]} {player["lastName"]}**'
-                rebounds_statline += f': {rebounds_leader_value} REBS'
+                    rebounds_statline += ' **{} {}**'.format(player["firstName"], player["lastName"])
+                rebounds_statline += ': {} REBS'.format(rebounds_leader_value)
                 
                 # Grab assists leader information
-                assists_statline = '\n'
+                assists_statline = '\n\n'
                 assists_leader_value = boxscore['stats'][team_stats_key]['leaders']['assists']['value']
                 for index, player in enumerate(boxscore['stats'][team_stats_key]['leaders']['assists']['players']):
                     if index > 0:
                         assists_statline += ' /'
-                    assists_statline += f' **{player["firstName"]} {player["lastName"]}**'
-                assists_statline += f': {assists_leader_value} ASTS'
+                    assists_statline += ' **{} {}**'.format(player["firstName"], player["lastName"])
+                assists_statline += ': {} ASTS'.format(assists_leader_value)
                 
                 if team_score < opponents_score:
                     result = 'lose to'
                 else:
                     result = 'defeat'
                 
-                title = f'[Post Game Thread] {TEAM_NAMES[TEAM]} {result} {TEAM_NAMES[opponents_name]} {team_score} - {opponents_score}'
-                selftext = f'* [Box Score](https://www.nba.com/games/{todays_game["gameUrlCode"]}#/boxscore)'
-                selftext += '\n\nStats Leaders'
+                title = '[Post Game Thread] {} {} {} {} - {}'.format(TEAM_NAMES[TEAM], result, TEAM_NAMES[opponents_name], team_score, opponents_score)
+                selftext = '* [Box Score](https://www.nba.com/games/{}#/boxscore)'.format(todays_game["gameUrlCode"])
+                selftext += '\n\nStat Leaders'
                 selftext += points_statline
                 selftext += rebounds_statline
                 selftext += assists_statline
                 result = reddit.subreddit('heat').submit(title, selftext=selftext, flair_id='d79dc9aa-cf0d-11e2-9b1b-12313d163d8f')
                 #result = reddit.subreddit('testingground4bots').submit(title, selftext=selftext)
-                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game ended thread posted')
+                thread_posted = True
+                print('[{}]: Game ended thread posted'.format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
                 break
             
-            if todays_game['period']['current'] >= 4 and todays_game['statusNum'] == 2 and not todays_game['clock']:
-                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game might have ended')
+            elif todays_game['period']['current'] >= 4 and todays_game['statusNum'] == 2 and not todays_game['clock']:
+                print('[{}]: Game might have ended'.format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
                 time.sleep(3)
-            elif todays_game['period']['current'] >= 4 and todays_game['clock'] and int(todays_game['clock'].replace(':', '').split('.')[0]) < 30:
-                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game is almost over')
+            elif todays_game['period']['current'] >= 4 and todays_game['clock'] and int(todays_game['clock'].replace(':', '').split('.')[0]) < 40:
+                print('[{}]: Game is almost over'.format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
                 time.sleep(10)
             else:
-                print(f'[{datetime.now().strftime("%a, %b %d, %Y %I:%M %p")}]: Game not over yet')
+                print('[{}]: Game not over yet'.format(datetime.now().strftime("%a, %b %d, %Y %I:%M %p")))
                 time.sleep(90)
-        time.sleep(600)
     
