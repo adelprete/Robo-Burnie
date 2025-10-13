1349560bc1370c3e9a0bbaa79e49a7cfdc5adebc
Anthony Delprete <adelprete87@gmail.com>
2025-10-13 16:44:30 -0400

    new auth lookup

--- src/robo_burnie/scripts/schedule_sidebar.py
+++ src/robo_burnie/scripts/schedule_sidebar.py
@@ -52,7 +52,9 @@ def _get_google_calendar_service() -> Resource:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("creds.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "google_credentials.json", SCOPES
+            )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open("token.json", "w") as token:
